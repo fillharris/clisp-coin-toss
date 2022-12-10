139df2e4d4e366dbaf787a846d5b82d81d7a481b
Philip Harris <ptharris79@gmail.com>
2022-12-09 22:09:12 -0500

    New commit

---
+++ src/main.lisp
@@ -0,0 +1,26 @@
+(defpackage coin-toss
+  (:use :cl))
+(in-package :coin-toss)
+
+; toss-coin function
+(defun toss-coin ()
+    (let ((number (random 2 (make-random-state t))))
+      (if (= number 0)
+          "heads"
+          "tails")))
+
+(defun prompt ()
+  (format t "Please enter heads or tails: ")
+  (force-output)
+
+  (let ((guess (string-downcase (read-line))))
+    (if (or (string= guess "heads")
+            (string= guess "tails"))
+        guess
+        (prompt))))
+
+(defun game ()
+  (if (string= (prompt)
+               (toss-coin))
+      (format nil "You win!~%")
+      (format nil "You loose!~%")))
